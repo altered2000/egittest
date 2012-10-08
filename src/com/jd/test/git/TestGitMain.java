9601e9fb002dce46b1b016764aa67cb59e5f6c61
altered2k <fengsonghao@360buy.com>
2012-10-08 11:37:04 +0800

    222

--- src/com/jd/test/git/TestGitMain.java
+++ src/com/jd/test/git/TestGitMain.java
@@ -4,4 +4,21 @@ package com.jd.test.git;
 public class TestGitMain {
  private String name;
  private String id;
+
+public String getName() {
+    return name;
+}
+
+public void setName(String name) {
+    this.name = name;
+}
+
+public String getId() {
+    return id;
+}
+
+public void setId(String id) {
+    this.id = id;
+}
+ 
 }
